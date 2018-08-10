6e48955ab4e775766809cebec9274b67487e6c85
scotte <1746650525@qq.com>
2018-08-11 00:11:38 +0800

    pythonall

---
+++ python/untitled0.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  7 15:59:02 2018
+
+@author: scotte
+"""
+
+#p = dict(x = 1, y=2)
+
+#def f(x, y, z):
+#    return x+y+z
+
+#print(f(z=3,**p))
+
+
+import tensorflow as tf
+
+print(tf.__version__)
+print(tf.__path__)
